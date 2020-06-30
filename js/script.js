70492519cd6a6b64bdca221dfbf2a8dc46ba61aa
NazarSagun <nazar.sagun@gmail.com>
2020-06-30 15:45:02 +0200

    fourth commit

--- js/script.js
+++ js/script.js
@@ -109,4 +109,57 @@ window.addEventListener('DOMContentLoaded', function(){
         document.body.style.overflow = 'hidden';
     });
 
+    // Form
+
+    let message = {
+        loading: 'Loading...',
+        success: 'Thanks! We will contact you soon!',
+        failure: 'Something went wrong...'
+    };
+
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        footerForm = document.getElementById('form'),
+        footerInput = footerForm.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+        statusMessage.classList.add('status');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        form.appendChild(statusMessage);
+        footerForm.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader ('Content-Type', 'application/json; charset=utf-8');
+
+        let formData = new FormData(form);
+        request.send(formData);
+
+        let obj = {};
+        formData.forEach(function(value, key) {
+            obj[key] = value;
+        });
+        let json = JSON.stringify(obj);
+
+        request.send(json);
+
+        request.addEventListener('readystatechange', function() {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if (request.readyState === 4 && request.status == 200) {
+                statusMessage.innerHTML = message.success;
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+
+            for (let i = 0; i < input.length, footerInput.length; i++) {
+                input[i].value = '';
+                footerInput[i].value = '';
+            }
+    });
+
+
 });
